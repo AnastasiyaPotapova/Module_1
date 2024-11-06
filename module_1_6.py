96f849b3dab02407724c94d8cbc415a93d4c7d2d
AnastasiyaPotapova <owl4price@gmail.com>
2024-11-07 00:25:11 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,9 +1,17 @@
-a = ", количество выполненных задач:"
-firts_count = 12
-b = ", количество затраченных часов:"
-second_count = 1.5
-c = "Название курса:"
-third_count = 'Python'
-d = ", время, затраченное на одно задание:"
-fourth_count = float (second_count/firts_count)
-print (c, third_count, a, firts_count, b, second_count, d, fourth_count)
+my_dict = {'Asia':1991, 'Alex':1992, 'Kate':1995}
+print(my_dict)
+print(my_dict['Alex'])
+my_dict['Anna'] = 1985
+print(my_dict)
+print(my_dict['Anna'])
+my_dict.update({'Jake':1987, 'Stan':1983})
+print(my_dict)
+del my_dict['Asia']
+print(my_dict)
+my_set = {1, 2, 3, 1, 4, 'string', 3, 'string'}
+print(my_set)
+my_set.add(5)
+my_set.add('red')
+print(my_set)
+my_set.discard(3)
+print(my_set)
