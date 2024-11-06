54e5a20a29d951fbb95d6f1b5cac5f1113625492
AnastasiyaPotapova <owl4price@gmail.com>
2024-11-07 00:30:03 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,9 @@
+a = ", количество выполненных задач:"
+firts_count = 12
+b = ", количество затраченных часов:"
+second_count = 1.5
+c = "Название курса:"
+third_count = 'Python'
+d = ", время, затраченное на одно задание:"
+fourth_count = float (second_count/firts_count)
+print (c, third_count, a, firts_count, b, second_count, d, fourth_count)
